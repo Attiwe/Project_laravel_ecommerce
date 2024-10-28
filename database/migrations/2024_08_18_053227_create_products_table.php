a62b75b6fac27623313e2e9702faef01565d0f0d
EbrahimSamy <ebrahimateiwi@gmail.com>
2024-10-28 21:28:14 +0300

    Update 2024_08_18_053227_create_products_table.php

--- database/migrations/2024_08_18_053227_create_products_table.php
+++ database/migrations/2024_08_18_053227_create_products_table.php
@@ -23,7 +23,7 @@ return new class extends Migration
             $table->boolean('is_active')->default(true);
             $table->boolean('is_featured')->default(false);
             $table->boolean('in_stock')->default(true);
-            $table->boolean('on_sale')->default(false); //معروض للبيع
+            $table->boolean('on_sale')->default(false);  
             $table->timestamps();
         });
     }
