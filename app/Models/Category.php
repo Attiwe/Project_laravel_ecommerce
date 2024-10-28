30975799117b68c6669ba5fe388edf992f9ae277
EbrahimSamy <ebrahimateiwi@gmail.com>
2024-10-28 21:37:34 +0300

    Update Category.php

--- app/Models/Category.php
+++ app/Models/Category.php
@@ -10,8 +10,7 @@ class Category extends Model
     use HasFactory;
     protected $fillable = ['name','slug','image','is_active'];
 
-//     هذه دالة تُعرّف علاقة بين الموديل Category والموديل الآخر الذي يمثل المنتجات (Products).
-// العلاقة هنا تُشير إلى أن فئة واحدة (Category) يمكن أن تحتوي على العديد من المنتجات (Product). هذه العلاقة تُعرف باسم علاقة "واحد إلى العديد" (One-to-Many).
+ 
     public function products(){
         return $this->hasMany(Product::class);
     }
